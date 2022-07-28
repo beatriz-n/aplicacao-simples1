ca460f4a46b733d1bc4ff3a242f789f239406c3e
beatriz <beatriznespolid@gmail.com>
2022-07-28 12:53:07 -0300

    buttons = delete and set

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,16 +1,20 @@
 <template>
 <div class="container grid-xs py-2 ">
   <img class="img-responsive img" alt="Vue logo" src="./assets/logo.png"><br>
-  <div>Esta é uma pagina usando spectre</div><br>
-  <form @submit.prevent="addTodo(todo)">
-    <div class="input-group">
-      <input type="text" v-model="todo.description" class="form-input" placeholder="Nova tarefa">
-      <button class="btn btn-primary input-group-btn">Adicionar</button>
+   <div class id="app">
+    <div class="container grid-xs py-2">
+      <div><b>Digite lembretes:</b></div>
+      <form @submit.prevent="addTodo(todo)">
+        <div class="input-group">
+          <input type="text" v-model="todo.description" class="form-input" placeholder="Novo todo">
+          <button class="btn btn-primary input-group-btn">Adicionar</button>
+        </div>
+      </form>
+      <div class="todo-list">
+        <todo v-for="t in todos" :key="t.id" @toggle="toggleTodo" @remove="removeTodo" :todo="t"/>
+      </div>
     </div>
-     <div class="todo-list">
-        <todo v-for="t in todos" :key="t.id" :todo="t"/>
-     </div>
-  </form>
+  </div>
 </div>
 </template>
 
@@ -28,6 +32,21 @@ export default {
       todo.id = Date.now()
       this.todos.push(todo)
       this.todo = { checked: false }
+    },
+
+    toggleTodo (todo) {
+      const index = this.todos.findIndex(item => item.id === todo.id)
+      if (index > -1) {
+        const checked = !this.todos[index].checked
+        this.todos[index].checked = checked
+      }
+    },
+    removeTodo (todo) {
+      const index = this.todos.findIndex((item) => item.id === todo.id)
+
+      if (index > -1) {
+        this.todos.splice(index, 1)// não precisa reatribuir ao this.todos
+      }// pois 'splice' já altera o array original
     }
   }
 }
@@ -39,7 +58,7 @@ max-width: 200px;
 margin: 0 auto;
 }
 .todo-list{
-
+ padding-top: 2rem;
 }
 </style>
 
--- todo/src/components/todo-temp.vue
+++ todo/src/components/todo-temp.vue
@@ -1,18 +1,22 @@
 <template>
- <div class="tile flex-centered">
-    <div class="titl-icon">
-        <i class="icon icon-time"></i>
+<!-- toda vez que tiver : posso colocar javaScript -->
+ <div class="tile" :class="{checked: todo.checked}">
+    <div class="tile-icon">
+      <i class="icon" :class="todo.checked ? 'icon-check' : 'icon-time'"></i>
     </div>
-        <dir class="tile-content">
-            <div class="tile-subtitle">{{todo.description}}</div>
-        </dir>
-        <div class="tile-action">
-            <button class="btn btn-link">Concluir</button>
-            <button class="btn btn-link">
-                <span class="text-error">Remover</span>
-            </button>
-        </div>
+    <div class="tile-content">
+      <p class="tile-subtitle">{{ todo.description }}</p>
     </div>
+    <div class="tile-action">
+      <button @click="$emit('toggle', todo)" class="btn btn-link">
+        <span v-if="todo.checked">Desmarcar</span>
+        <span v-else>Concluído</span>
+      </button>
+      <button @click="$emit('remove', todo)" class="btn btn-link">
+        <span class="text-error">Remover</span>
+      </button>
+    </div>
+  </div>
 </template>
 <script>
 export default {
@@ -21,3 +25,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.checked{
+    text-decoration: line-through;
+    color: white;
+}
+</style>
